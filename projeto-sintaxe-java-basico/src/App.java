a341c25a0ceb99b1881591e902dc8fbdc1b1243d
raulmaciel0 <raul.maciel015@gmail.com>
2023-09-19 20:20:47 -0300

    meu projeto conta banco

---
+++ projeto-sintaxe-java-basico/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite algo: ");
+        String ola = sc.nextLine();
+
+        System.out.println(ola);
+    }
+}
--- teste-java-terminal/src/AboutMe.java
+++ teste-java-terminal/src/AboutMe.java
@@ -1,14 +1,15 @@
+
+
 public class AboutMe {
 
 public static void main(String[] args) {
-    String nome = args [0];
-    String sobrenome = args [1];
-    int idade = Integer.valueOf(args[2]);
-    double altura = Double.valueOf(args[3]);
+    
+    int numero1 = 1;
 
-    System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-    System.out.println("Tenho " + idade + " anos");
-    System.out.println("Minha altura é " + altura + " cm");
+      String numero2 = "2";
+
+      System.out.println(numero1+numero2); 
+
 }
 
 }
